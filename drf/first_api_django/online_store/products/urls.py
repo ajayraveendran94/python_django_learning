622f7a6e04e1bc1cca4b3b98b8a65aaa6aba61d5
Ajay Raveendran <ajayraveendran@Ajays-MacBook-Air.local>
2024-10-14 23:23:37 +0530

    First assignment DRF

--- drf/first_api_django/online_store/products/urls.py
+++ drf/first_api_django/online_store/products/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import ProductDetailView, ProductListView
+#from .views import ProductDetailView, ProductListView
+from .views import (manufacturer, manufacture, manufacture_detail, 
+                    product_list, product_detail)
 
 urlpatterns=[
-    path("", ProductListView.as_view(), name="product-list"),
-    path("products/<int:pk>/", ProductDetailView.as_view(), name="product-details")
+    path("products/", product_list, name = "prodcut_list"),
+    path("products/<int:pk>", product_detail, name = "prodcut_detail"),
+    path("manufacturer/<str:status>", manufacturer, name = "manufacturer_detail"),
+    path("manufactures/", manufacture, name = "manufacturer_list"),
+    path("manufacture_detail/<int:pk>", manufacture_detail, name = "manufacture_detail"),
+    # path("", ProductListView.as_view(), name="product-list"),
+    # path("products/<int:pk>/", ProductDetailView.as_view(), name="product-details")
 ]
--- drf/first_api_django/online_store/products/views.py
+++ drf/first_api_django/online_store/products/views.py
@@ -3,6 +3,92 @@
 from django.http import JsonResponse
 from .models import Product, Manufacturer
 
+
+def product_list(request):
+    products = Product.objects.all()
+    #data = {"products": list(products.values("pk", "name"))}
+    data = {"products": list(products.values())}
+    response = JsonResponse(data)
+    return response
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk = pk)
+        data = {"product": {
+            "name": product.name,
+            "manufacturer": product.manufacturer.name,
+            "description": product.description,
+            "image": product.image.url,
+            "price": product.price,
+            "shippping_cost": product.shipping_cost,
+            "quantity": product.quantity,
+        }}
+        response = JsonResponse(data)
+    except Product.DoesNotExist:
+        response = JsonResponse({
+            "error":{
+                "code": 404,
+                "message": "product not found"
+            }
+        }, status=404)
+    return response
+
+#My Solution
+def manufacturer(request, status):
+    try:
+        if status == "true":
+            manufacturer = Manufacturer.objects.filter(active = True)
+        else:
+            manufacturer = Manufacturer.objects.all()
+        data_set = []
+        if manufacturer:
+            for manufact in manufacturer:
+                data = { "manufacturer": {
+                    "name": manufact.name,
+                    "location": manufact.location,
+                    "status": manufact.active,
+                    "products": list(manufact.products.values())
+                }}
+                data_set.append(data)
+        print(data_set)
+        response = JsonResponse(data_set,safe= False)
+    except Manufacturer.DoesNotExist:
+        response = JsonResponse({
+            "error":{
+                "code": 404,
+                "message": "Manufacturer not found"
+            }
+        }, status=404)
+    return response
+
+#Answer
+def manufacture(request):
+    manufacturers = Manufacturer.objects.all()
+    data = {"manufacturer": list(manufacturers.values())}
+    response = JsonResponse(data)
+    return response
+
+def manufacture_detail(request, pk):
+    try:
+        manufacturer = Manufacturer.objects.filter(active=True).get(pk = pk)
+        manufacture_products = manufacturer.products
+        data = {"manufacturer": {
+            "name": manufacturer.name,
+            "location": manufacturer.location,
+            "status": manufacturer.active,
+            "products": list(manufacture_products.values())
+        }}
+        response = JsonResponse(data)
+    except Manufacturer.DoesNotExist:
+        response = JsonResponse({
+            "error":{
+                "code": 404,
+                "message": "Manufacturer not found"
+            }
+        }, status=404)
+    return response
+
+
 # Create your views here.
 
 # class ProductDetailView(DetailView):
